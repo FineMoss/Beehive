a8fd9cdfe86286f26f27c73a5661a9a62c120f6d
Jake Stephens <jake@Jakes-MBP.fios-router.home>
2018-10-17 21:50:25 -0400

    working version of View.py

--- Beehive/View.py
+++ Beehive/View.py
@@ -2,11 +2,11 @@
 # Created by Jake Stephens
 
 
-from tkinter import *
+from   tkinter import *
 import Control as c
 
 
-class Application(tk.Frame):
+class Application(Frame):
 
     # initializes the window
     def __init__(self, master=None):
@@ -20,30 +20,30 @@ class Application(tk.Frame):
         # buttons
 
         # quit button
-        self.quit_button                  = tk.Button(self)
+        self.quit_button                  = Button(self)
         self.quit_button["text"]          = "QUIT"
         self.quit_button["bg"]            = "red"
         self.quit_button["command"]       = self.quit
 
         # archive button
-        self.archive_button               = tk.Button(self)
+        self.archive_button               = Button(self)
         self.archive_button["text"]       = "Run Archive Report"
         self.archive_button["command"]    = self.archive
 
         # clear all button
-        self.clear_button                 = tk.Button(self)
+        self.clear_button                 = Button(self)
         self.clear_button["text"]         = "Clear All Fields"
         self.clear_button["command"]      = self.clear
 
         # confirm button
-        self.confirm_button               = tk.Button(self)
+        self.confirm_button               = Button(self)
         self.confirm_button["text"]       = "Confirm Archive"
         self.confirm_button["fg"]         = "green"
         self.confirm_button["command"]    = self.confirm
         self.confirm_button["state"]      = "disable"
 
         # cancel button
-        self.cancel_button                = tk.Button(self)
+        self.cancel_button                = Button(self)
         self.cancel_button["text"]        = "Cancel Archive"
         self.cancel_button["fg"]          = "red"
         self.cancel_button["command"]     = self.cancel
@@ -52,37 +52,37 @@ class Application(tk.Frame):
         # entries
 
         # target entry 
-        self.current_entry                = tk.Entry(self)
+        self.current_entry                = Entry(self)
         self.current_entry["width"]       = 40
 
         # archive entry
-        self.target_entry                 = tk.Entry(self)
+        self.target_entry                 = Entry(self)
         self.target_entry["width"]        = 40
 
         # date entry
-        self.date_entry                   = tk.Entry(self)
+        self.date_entry                   = Entry(self)
         self.date_entry["width"]          = 10
 
         # labels
 
         # target label
-        self.current_label                = tk.Label(self)
+        self.current_label                = Label(self)
         self.current_label["text"]        = "Folder to be Archived"
         self.current_label["pady"]        = 5
 
 
         # archive label
-        self.target_label                 = tk.Label(self)
+        self.target_label                 = Label(self)
         self.target_label["text"]         = "Archive To This Location"
         self.target_label["pady"]         = 5
 
         # date label
-        self.date_label                   = tk.Label(self)
+        self.date_label                   = Label(self)
         self.date_label["text"]           = "Enter a Date (MM/DD/YYYY)"
         self.date_label["pady"]           = 5
 
         # text field
-        self.text_field                   = tk.Text(self)
+        self.text_field                   = Text(self)
         self.text_field["height"]         = 30
         self.text_field["padx"]           = 5
         self.text_field["yscrollcommand"] = True
@@ -109,7 +109,7 @@ class Application(tk.Frame):
         self.date_label.grid(row = 4, column = 0, columnspan = 2)
 
         # text field
-        self.text_field.grid(row = 0, column = 2, rowspan = 30, sticky = tk.W+tk.E+tk.N+tk.S)
+        self.text_field.grid(row = 0, column = 2, rowspan = 30, sticky = W+E+N+S)
 
 
 
