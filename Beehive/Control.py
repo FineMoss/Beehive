9614b0f2e517bd6632de10e57f49b7c3f2491431
Jake Stephens <jake@Jakes-MBP.fios-router.home>
2018-10-18 23:39:34 -0400

    implemented Cotrol.py functions

--- Beehive/Control.py
+++ Beehive/Control.py
@@ -9,6 +9,22 @@ class Control():
         self.__frame = frame
 
 
+    def archive(self):
+        print("TODO: Control.archive()")
+
+
+    def confirm(self):
+        print("TODO: Control.confirm()")
+
+
+    def cancel(self):
+        self.text_field["state"] = "normal"
+        self.text_field.delete("1.0", "end")
+        self.text_field["state"] = "disabled"
+        self.unlock_fields()
+        print("MORE TODO: Control.cancel()")
+
+
     def clear(self):
         self.__frame.current_entry.delete("0", "end")
         self.__frame.target_entry.delete("0", "end")
@@ -18,17 +34,21 @@ class Control():
         self.__frame.text_field["state"] = "disabled"
 
 
-    def cancel(self):
-        print("TODO: Control.cancel()")
+    def __lock_fields(self):
+        self.__frame.current_entry["state"]  = "disabled"
+        self.__frame.target_entry["state"]   = "disabled"
+        self.__frame.date_entry["state"]     = "disabled"
+        self.__frame.archive_button["state"] = "disabled"
+        self.__frame.clear_button["state"]   = "disabled"
+        self.__frame.confirm_button["state"] = "normal"
+        self.__frame.cancel_button["state"]  = "normal"
 
 
-    def confirm(self):
-        print("TODO: Control.confirm()")
-
-
-    def archive(self):
-        print("TODO: Control.archive()")
-
-
-    def __my_private_method(self):
-        print("NODO")
+    def __unlock_fields(self):
+        self.__frame.current_entry["state"]  = "normal"
+        self.__frame.target_entry["state"]   = "normal"
+        self.__frame.date_entry["state"]     = "normal"
+        self.__frame.archive_button["state"] = "normal"
+        self.__frame.clear_button["state"]   = "normal"
+        self.__frame.confirm_button["state"] = "disable"
+        self.__frame.cancel_button["state"]  = "disable"
