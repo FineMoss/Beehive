4a9b098bdf67d40320fe631d67bb881c42d12500
Jake Stephens <jake@Jakes-MBP.fios-router.home>
2018-10-21 22:23:56 -0400

    semi-final run_report impl

--- Beehive/Model.py
+++ Beehive/Model.py
@@ -1,10 +1,10 @@
 
 # Created by Jake Stephens
 
-
+# TODO import only what is needed
 import os
+import time
 # import sys
-# import shutil
 # import datetime
 
 
@@ -15,40 +15,64 @@ class Model():
     def __init__(self, reports_dir=".reports", extension=".report"):
         self.reports_dir = reports_dir
         self.extension   = extension
-        # make sure there is a reports direcory
-        if not os.path.exists(self.reports_dir):
-            os.makedirs(self.reports_dir)
 
     # creates a report with report_name.extension in reports_dir and
     # adds path of files from source_path that the fit the conditions to the report
-    def run_report(self, report_name="", source_dir="", conditions={}):
+    def run_report(self, source_dir, report_name="", conditions={}):
+        # initalize variables
+        report_name = self.__name_report(report_name)
         report_path = os.path.join(self.reports_dir, report_name+self.extension)
-        report      = open(report_path, "w")
         file_count  = 0
-        # recursively walk the directory tree
-        for root, dirs, files in os.walk(source_dir, onerror=self.handle_error):
-            for file in files:
-                # if the file meets the conditions
-                if self.conditions_apply(conditions, file):
-                    report.write(os.path.join(root, file)+"\n")
-                    file_count+=1
+        # make sure there is a reports directory
+        if not os.path.exists(self.reports_dir):
+            os.makedirs(self.reports_dir)
+        # create the report
+        with open(report_path, "w") as report:
+            # recursively walk the directory tree
+            for root, dirs, files in os.walk(source_dir, onerror=self.__handle_error):
+                for file in files:
+                    # if the file meets the conditions
+                    if self.conditions_apply(conditions, file):
+                        # add the path to the report
+                        report.write(os.path.join(root, file)+"\n")
+                        file_count+=1
+            # add file count and the source_dir for later use
+            report.write("!! File Count: "+str(file_count)+" !!\n")
+            report.write("source_dir="+source_dir+"\n")
+        return report_path
 
-        report.write("!! File Count: "+str(file_count)+" !!\n")
-        report.close()
+    # moves all of the files from the report to a destination directory
+    def move_files(self, report_name, destination_dir):
+        # TODO
 
+        with open(report_path, "w") as report:
+            for line in report:
+                # TODO filter out "!!" comments
 
+                # TODO should be rel path or use absolute path
+                dst = destination_dir
+                os.rename(line, dst)
 
 
     def conditions_apply(self, conditions, file):
+        # TODO
         return True
 
 
-    def handle_error(self, Error_instance):
+    def __handle_error(self, error_instance):
+        # TODO
         print("!! ERROR: could not find file !!")
 
 
+    def __name_report(self, report_name):
+        if report_name is "":
+            return time.strftime("%Y%m%d-%H%M%S")
+        else:
+            return report_name
+
 
 # testing purposes 
+# TODO: remove
 if __name__ == "__main__":
     m = Model(reports_dir=".beehive_reports")
-    m.run_report(report_name="my_report", source_dir="test")
+    m.run_report("test", report_name="my_report")
