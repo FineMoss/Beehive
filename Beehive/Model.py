6b46de4f876ed53250c827e8ad4582f8756e2e60
Jake Stephens <jake@Jakes-MBP.fios-router.home>
2019-01-12 00:49:17 -0500

    Complete beta v2.0

--- Beehive/Model.py
+++ Beehive/Model.py
@@ -1,11 +1,11 @@
 
 # Created by Jake Stephens
 
-# TODO import only what is needed
-import os
+# only import what is needed
 import time
-# import sys
-# import datetime
+import os.path
+from os     import makedirs
+from shutil import move
 
 
 class Model():
@@ -25,54 +25,81 @@ class Model():
         file_count  = 0
         # make sure there is a reports directory
         if not os.path.exists(self.reports_dir):
-            os.makedirs(self.reports_dir)
+            makedirs(self.reports_dir)
         # create the report
         with open(report_path, "w") as report:
             # recursively walk the directory tree
             for root, dirs, files in os.walk(source_dir, onerror=self.__handle_error):
                 for file in files:
+                    file_path = os.path.join(root, file)
                     # if the file meets the conditions
-                    if self.conditions_apply(conditions, file):
+                    if self.__conditions_apply(conditions, file_path):
                         # add the path to the report
-                        report.write(os.path.join(root, file)+"\n")
+                        report.write(file_path+"\n")
                         file_count+=1
             # add file count and the source_dir for later use
             report.write("!! File Count: "+str(file_count)+" !!\n")
-            report.write("source_dir="+source_dir+"\n")
+
         return report_path
 
     # moves all of the files from the report to a destination directory
-    def move_files(self, report_name, destination_dir):
-        # TODO
+    def move_files(self, report_path, destination_dir):
+        # TODO try catch OS_Error __handle_error
+        # TODO if file already exists in dir - rename to new_file(1)
+        if os.path.exists(report_path):
+            with open(report_path, "r") as report:
+                for line in report.readlines():
+                    if line[:2] != "!!":
+                        line = line.rstrip()
+                        # TODO should be rel path or use absolute path
+                        common_path = os.path.commonpath([destination_dir, line])
+                        cutoff = len(common_path.split("/"))
+                        dst = os.path.join(destination_dir, *line.split("/")[cutoff:-1])
+                        if not os.path.exists(dst):
+                            # TODO make sure (try catch) dst is not a file
+                            makedirs(dst)
+                        move(line, dst)
+        else:
+            __throw_error()
 
-        with open(report_path, "w") as report:
-            for line in report:
-                # TODO filter out "!!" comments
-
-                # TODO should be rel path or use absolute path
-                dst = destination_dir
-                os.rename(line, dst)
-
-
-    def conditions_apply(self, conditions, file):
-        # TODO
-        return True
-
-
-    def __handle_error(self, error_instance):
-        # TODO
-        print("!! ERROR: could not find file !!")
-
-
+    # create name for the report
     def __name_report(self, report_name):
-        if report_name is "":
+        if report_name == "":
             return time.strftime("%Y%m%d-%H%M%S")
         else:
             return report_name
 
+    # verify that all conditions are met for a file to be added to the report
+    def __conditions_apply(self, conditions, file):
+        all_conditions_apply = True
+        # iterate 
+        for key in conditions:
+            # pass off condition types
+            if conditions[key](key, file) == False:
+                all_conditions_apply = False
 
-# testing purposes 
-# TODO: remove
-if __name__ == "__main__":
-    m = Model(reports_dir=".beehive_reports")
-    m.run_report("test", report_name="my_report")
+        return all_conditions_apply
+
+    # return true if the file was last mod before the given date
+    # else return false
+    def date_condition(self, date, file):
+        # get time last modified in epoch format
+        mod_epoch = os.path.getmtime(file)
+        # convert input date to epoch format
+        # TODO check for errors in format
+        date_struct = time.strptime(date, "%m/%d/%Y")
+        date_epoch = time.mktime(date_struct)
+
+        if date_epoch > mod_epoch:
+            return True
+        else:
+            return False
+
+    
+    def __throw_error():
+        # TODO
+        print("!! ERROR: could not find file !!")
+
+    def __handle_error(self, error_instance):
+        # TODO
+        print("!! ERROR: could not find file !!")
